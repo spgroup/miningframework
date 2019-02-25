300b70274e577852bc48c096bbdf5e5e3eb77564
João Victor <joao.coutinho9@gmail.com>
2019-02-25 12:47:39 -0300

    Cloning remote repositories

--- src/main/MiningFramework.groovy
+++ src/main/MiningFramework.groovy
@@ -1,18 +1,55 @@
 @Grab('com.xlson.groovycsv:groovycsv:1.3')
 import static com.xlson.groovycsv.CsvParser.parseCsv
 import java.io.File
+import java.nio.file.Files 
+import java.nio.file.Paths
 import java.util.ArrayList;
 
 class MiningFramework {
 
     private ArrayList<Project> projectList
+    private final String LOCAL_PROJECT_PATH = 'localProject'
 
     public MiningFramework(ArrayList<Project> projectList) {
         this.projectList = projectList
     }
 
     public void start() {
+        for (project in projectList) {
+            if (project.isRemote())
+                cloneRepository(project)
+        }
+    }
+
+    private void cloneRepository(Project project) {
+
+        println "Cloning repository ${project.getName()} into ${LOCAL_PROJECT_PATH}"
+
+        if(Files.exists(Paths.get(LOCAL_PROJECT_PATH))) {
+            File projectDirectory = new File(LOCAL_PROJECT_PATH)
+            delete(projectDirectory)
+        }
+
+        Process gitClone = new ProcessBuilder('git', 'clone', project.getPath(), LOCAL_PROJECT_PATH).start()
+        gitClone.waitFor()
         
+    }
+
+    private delete(File file) {
+        if (!file.isDirectory())
+            file.delete()
+        else {
+            if (file.list().length == 0) 
+                file.delete()
+            else {
+                String[] files = file.list()
+                for (temp in files) {
+                    delete(new File(file, temp))
+                }
+                if (file.list().length == 0) 
+                    file.delete()
+            }
+        }
     }
 
     static main(args) {
@@ -28,7 +65,7 @@ class MiningFramework {
     static ArrayList<Project> getProjectList() {
         ArrayList<Project> projectList = new ArrayList<Project>()
 
-        String projectsFile = new File('../../projects.csv').getText()
+        String projectsFile = new File('projects.csv').getText()
         def iterator = parseCsv(projectsFile)
         for (line in iterator) {
             String name = line[0]
--- src/main/Project.groovy
+++ src/main/Project.groovy
@@ -2,25 +2,35 @@ class Project {
     
     private String name
     private String path
+    private boolean remote
 
     public Project(String name, String path) {
         this.name = name
         this.path = path
+        this.remote = path.startsWith('https://github.com/')
     }
 
     public String getName() {
         return name
     }
 
-    public String getPath() {
-        return path
-    }
-
     public void setName(String name) {
         this.name = name
     }
 
+    public String getPath() {
+        return path
+    }   
+
     public void setPath(String path) {
         this.path = path
+    }
+
+    public boolean isRemote() {
+        return remote
+    }
+
+    public void setRemote(boolean remote) {
+        this.remote = remote
     }
 }

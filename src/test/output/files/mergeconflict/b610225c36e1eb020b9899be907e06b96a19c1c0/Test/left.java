98b2fd04a691f0f69301f1b5def0ad4a8cf55263
Rafael Mota <rma7@cin.ufpe.br>
2019-04-06 21:17:13 -0300

    Add merge conflicts count tests

---
+++ src/test/output/files/mergeconflict/b610225c36e1eb020b9899be907e06b96a19c1c0/Test/left.java
@@ -0,0 +1,14 @@
+class Test {
+	public void mergeConflictOne() {
+		int x = 2;
+	}
+
+	public void mergeConflictTwo () {
+		int y = 2;
+	}
+
+	public void noConflict () {
+		int z = 0;
+		int x = 0;
+	}
+}
---
+++ src/test/output/files/mergeconflict/ea29fc17bdf20b40208ae39f88fd8aa2b2a5322b/Test/right.java
@@ -0,0 +1,19 @@
+class Test {
+	public void mergeConflictOne() {
+		int x = 2;
+	}
+
+	public void mergeConflictTwo () {
+		int y = 1;
+	}
+
+	public void noConflict () {
+		int z = 0;
+		int x = 0;
+		int y = 0;
+	}
+
+	public void multipleConflicts () {
+		int x = 1;
+	}
+}

021bf819cb4a350bb1115f938fbb90a8fe137e5f
Rafael Mota <rma7@cin.ufpe.br>
2019-04-28 00:31:44 -0300

    Add travis helper

---
+++ src/services/TravisHelper.groovy
@@ -0,0 +1,59 @@
+package services
+
+import main.util.HttpHelper
+import main.project.*
+import java.net.HttpURLConnection
+
+class TravisHelper {
+
+    private final String API_URL="https://api.travis-ci.org"
+    private String token; 
+    
+    TravisHelper(String accessKey) {
+        String url = "${API_URL}/auth/github"
+        HttpURLConnection request = HttpHelper.requestToApi(url, "POST")
+        def body = [github_token: accessKey]
+        request.setRequestProperty("Content-type", "application/json")
+        request.setDoOutput(true)
+
+        PrintStream printStream = new PrintStream(request.getOutputStream())
+        printStream.println(HttpHelper.jsonToString(body))
+
+        String responseMessage = request.getResponseMessage()
+
+        if (responseMessage != 'OK') {
+            throw new TravisHelperException('An error ocurred getting travis token')
+        }
+        def result = HttpHelper.responseToJSON(request.getInputStream())
+
+        this.token = result.access_token
+    }
+
+    public Map getProject(String owner, String projectName) {
+        String url = "${API_URL}/repos/${owner}/${projectName}"
+        HttpURLConnection request = HttpHelper.requestToApi(url, "GET", this.token)
+        String responseMessage = request.getResponseMessage()
+
+        if (responseMessage != 'OK') {
+            throw new TravisHelperException("An error ocurred trying to get ${project.getName()} project in travis")
+        }
+
+        return HttpHelper.responseToJSON(request.getInputStream())
+    }
+
+    public void enableTravis(Integer travisRepoId)  {
+        String url = "${API_URL}/hooks"
+        HttpURLConnection request = HttpHelper.requestToApi(url, "PUT", this.token)
+        def body = [hook: [ id: travisRepoId, active: true ]]
+        request.setRequestProperty("Content-type", "application/json")
+        request.setDoOutput(true)
+
+        PrintStream printStream = new PrintStream(request.getOutputStream())
+        printStream.println(HttpHelper.jsonToString(body))
+        String requestMessage = request.getResponseMessage()
+        if (requestMessage != 'Ok') {
+            throw new TravisHelperException("An error ocurred trying to enable travis a project")
+        }
+    }
+
+}
---
+++ src/services/TravisHelperException.groovy
@@ -0,0 +1,7 @@
+package services
+
+public class TravisHelperException extends Exception {
+    public TravisHelperException(String message) {
+        super(message)
+    }
+}

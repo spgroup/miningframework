44e35cc3d32911d93ad03db9a54d43fd1a929625
Rafael Mota <rma7@cin.ufpe.br>
2019-05-27 20:14:43 -0300

    Change assertion method in tests

--- src/test/MergeConflictCountTest.groovy
+++ src/test/MergeConflictCountTest.groovy
@@ -5,6 +5,8 @@ package test
 import static com.xlson.groovycsv.CsvParser.parseCsv
 import org.junit.Test
 import org.junit.BeforeClass
+import static test.Assert.assertEquals
+
 
 public class MergeConflictCountTest {
 
@@ -26,30 +28,30 @@ public class MergeConflictCountTest {
     @Test
     public void mergeConflictTest () {
         String commitSHA = 'b610225c36e1eb020b9899be907e06b96a19c1c0'
-        assert conflictFilesNumber.get(commitSHA) == '1'
-        assert mergeConflictsNumber.get(commitSHA) == '1'
+        assertEquals(conflictFilesNumber.get(commitSHA), '1')
+        assertEquals(mergeConflictsNumber.get(commitSHA), '1')
         
     }
 
     @Test
     public void multipleConflict () {
         String commitSHA = 'ea29fc17bdf20b40208ae39f88fd8aa2b2a5322b'
-        assert conflictFilesNumber.get(commitSHA) == '2'
-        assert mergeConflictsNumber.get(commitSHA) == '2'
+        assertEquals(conflictFilesNumber.get(commitSHA), '2')
+        assertEquals(mergeConflictsNumber.get(commitSHA),'2')
     }
 
     @Test
     public void multipleConflictInOneFile() {
         String commitSHA = 'c00294fb7135e5bdcd6f7c3417405aea75b66444'
-        assert conflictFilesNumber.get(commitSHA) == '1'
-        assert mergeConflictsNumber.get(commitSHA) == '2'
+        assertEquals(conflictFilesNumber.get(commitSHA), '1')
+        assertEquals(mergeConflictsNumber.get(commitSHA), '2')
     }
 
     @Test
     public void noMergeConflictTest () {
         String commitSHA = 'ef1a4c9095c04af5eab2e0c54f590c053c48d410'
-        assert conflictFilesNumber.get(commitSHA) == '0'
-        assert mergeConflictsNumber.get(commitSHA) == '0';
+        assertEquals(conflictFilesNumber.get(commitSHA), '0')
+        assertEquals(mergeConflictsNumber.get(commitSHA), '0')
     }
     
 }

0739cfdbd3e0a34c1f01ae0fb7302c28a5135709
Rafael Mota <rma7@cin.ufpe.br>
2019-09-14 13:06:54 -0300

    Add soot-analysis jar and multiple analysis

--- src/services/soot/SootRunner.groovy
+++ src/services/soot/SootRunner.groovy
@@ -17,35 +17,67 @@ class SootRunner {
     private String sootPath 
 
     private final String RESULTS_FILE_PATH = "/data/results-with-builds.csv"
+    private final String DATAFLOW_MODE = "dataflow"
+    private final String REACHABILITY_MODE = "reachability"
 
-
-    SootRunner(String outputPath, String sootProjectPath) {
+    SootRunner(String outputPath) {
         this.outputPath = outputPath
-        this.sootPath = sootProjectPath
     }
 
     public  void processScenarios() {
+        String resultHead = "project;class;method;commit;dataflow left right;dataflow right left;reachability left right;reachability right left\n"
+
         List<SootScenario> sootScenarios = SootScenario.readScenarios(outputPath + RESULTS_FILE_PATH);
 
         for (scenario in sootScenarios) {
 
             String filePath = scenario.getLinesFile(outputPath)
             String filePathReverse = scenario.getLinesFile(outputPath)
-            String className = scenario.getClassPath(outputPath)
+            String classPath = scenario.getClassPath(outputPath)
 
-            String analysisOutputLeftRight = runSootAnalysis(filePath, className)
-            String analysisOutputRightLeft = runSootAnalysis(filePathReverse, className)
+            println "Running left right dataflow analysis"
+            Process analysisLeftRightDataflow = runSootAnalysis(filePath, classPath, DATAFLOW_MODE)
+            boolean leftRightDataflow = hasSootFlow(analysisLeftRightDataflow)
+            println "Result: ${leftRightDataflow}"
+
+            println "Running right left dataflow analysis"
+            Process analysisRightLeftDataflow = runSootAnalysis(filePathReverse, classPath, DATAFLOW_MODE)
+            boolean rightLeftDataflow = hasSootFlow(analysisRightLeftDataflow)
+            println "Result: ${rightLeftDataflow}"
+
+            println "Running left right reachability analysis"
+            Process analysisLeftRightReachability = runSootAnalysis(filePath, classPath, DATAFLOW_MODE)
+            boolean leftRightReachability = hasSootFlow(analysisLeftRightReachability)
+            println "Result: ${leftRightReachability}"
+
+
+            println "Running right left reachability analysis"
+            Process analysisRightLeftReachability = runSootAnalysis(filePathReverse, classPath, DATAFLOW_MODE)
+            boolean rightLeftReachability = hasSootFlow(analysisRightLeftReachability)
+            println "Result: ${rightLeftReachability}"
+
         }
 
     }
 
-
-    public String runSootAnalysis (String filePath, String classPath) {
-        String cliParameters = "-csv ${filePath} -cp ${classPath}"
-        Process analysis = ProcessRunner
-            .runProcess(sootPath, "mvn", "exec:java" ,"-Dexec.mainClass=br.unb.cic.analysis.Main", "-Dexec.args=${cliParameters}")
-    
-        return analysis.getText()
+    public boolean hasSootFlow (Process sootProcess) {
+        boolean result = true
+        sootProcess.getInputStream().eachLine {
+            if (it.stripIndent() == "No conflicts detected") {
+                result = false
+            }
+        }
+        return result
     }
 
+    public Process runSootAnalysis (String filePath, String classPath, String mode) {
+        return ProcessRunner
+            .runProcess(".", "java", "-jar" ,"dependencies/soot-analysis.jar", "-csv", filePath, "-cp", classPath, "-mode", mode)
+    }
+
+    static main (args) {
+        SootRunner runner = new SootRunner("output")
+
+        runner.processScenarios()
+    }
 }

a3ba37dd363586dbbeb66a1666fb3fdd800a3b5b
leusonmario <leusonmario@gmail.com>
2019-11-01 21:46:31 +0100

    new commit filter to select merge scenarios without merge conflicts

---
+++ src/services/CommitFilterDynamicSemanticConflictImpl.groovy
@@ -0,0 +1,22 @@
+package services
+import main.interfaces.CommitFilter
+import static com.xlson.groovycsv.CsvParser.parseCsv
+
+import main.util.*
+import main.project.*
+
+class CommitFilterDynamicSemanticConflictImpl extends CommitFilterImpl {
+
+    @Override
+    public boolean applyFilter(Project project, MergeCommit mergeCommit) {
+        boolean hasMergeConflict = MergeHelper.hasMergeConflict(project, mergeCommit)
+
+        if (!hasMergeConflict){
+            super.applyFilter(project, mergeCommit)
+        }else{
+            return false
+        }
+
+    }
+
+}

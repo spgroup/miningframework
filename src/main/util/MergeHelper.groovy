7d8f3e0039ac9aa462ec1b9ec550defdd19b673a
leusonmario <leusonmario@gmail.com>
2019-12-24 01:04:34 +0100

    detecting whitespaces changes in commits pairs

--- src/main/util/MergeHelper.groovy
+++ src/main/util/MergeHelper.groovy
@@ -6,6 +6,7 @@ import java.util.regex.Matcher
 
 class MergeHelper {
     private static String CONFLICT_INDICATOR = "(CONFLICT)|(CONFLITO)"
+    private static String options = "--ignore-cr-at-eol --ignore-all-space --ignore-blank-lines --ignore-space-change"
 
     static public boolean hasMergeConflict (Project project, MergeCommit mergeCommit) {
         Process mergeSimulation = replayMergeScenario(project, mergeCommit)
@@ -22,18 +23,59 @@ class MergeHelper {
         return result
     }
 
-    public static boolean checkForNotEmptyDiff(Project project, MergeCommit mergeCommit){
-        Process diffAnalysis = performDiffAnalysis(project, mergeCommit)
-        boolean result = false
-        
-        if (diffAnalysis.getText() != ""){
-            result = true
-        }
+    public static boolean checkForEmptyDiff(Project project, MergeCommit mergeCommit){
+        Process diffAnalysis = performDiffAnalysisDeffault(project, mergeCommit, options)
+        boolean result = diffAnalysis.getText() == ""
         
         Process returnToMaster = returnToMaster(project)
         returnToMaster.waitFor()
     
         return result
+    }
+    public static ArrayList<Boolean> checkForEmptyDiffByParents(Project project, MergeCommit mergeCommit, String className){
+        ArrayList<Boolean> diffStatesByParents = new ArrayList<Boolean>();
+        
+        diffStatesByParents.add(performDiffAnalysisByParents(project, options, mergeCommit.getAncestorSHA(), mergeCommit.getLeftSHA(), className))
+        diffStatesByParents.add(performDiffAnalysisByParents(project, options, mergeCommit.getAncestorSHA(), mergeCommit.getRightSHA(), className))
+        diffStatesByParents.add(checkForEmptyDiff(project, mergeCommit))
+        
+        return diffStatesByParents
+    }
+
+    private static boolean getLocalPathForChangedClass(HashMap<String, Boolean> localChangedFiles, String reportChangedClass){
+        localChangedFiles.each{value, key ->
+            if (value.include(reportChangedClass.replaceAll(".","/"))){
+                return true
+            }
+        }
+        return false
+    }
+
+    public static boolean checkForEmptyDiffForFile(Project project, MergeCommit mergeCommit){
+        String[] changedFiles = getListFilesChanged(project, mergeCommit)
+        
+        HashMap<String, Boolean> changedFileEmptyDiffState = new HashMap<String, Boolean>();
+
+        changedFiles.each{value ->
+            changedFileEmptyDiffState[value] = performDiffAnalysisDeffault(project, mergeCommit, options+" "+value).getText() == ""
+        }
+
+        return changedFileEmptyDiffState
+    }
+
+    public static String[] getListFilesChanged (Project project, String parentOne, String parentTwo){
+        //Process diffAnalysis 
+        Process checkoutParentOne = ProcessRunner.runProcess(project.getPath(), 'git', 'checkout', parentOne)
+        checkoutParentOne.waitFor()
+
+        Process diffAnalysis = ProcessRunner.runProcess(project.getPath(), 'git', 'diff', parentTwo, "--name-only")
+        String[] filesList = diffAnalysis.getText().split("\n")
+        //String[] filesList = diffAnalysis.getText().split("\n")
+
+        Process returnToMaster = returnToMaster(project)
+        returnToMaster.waitFor()
+
+        return filesList
     }
 
     static private Process replayMergeScenario(Project project, MergeCommit mergeCommit) {
@@ -43,6 +85,32 @@ class MergeHelper {
         return ProcessRunner.runProcess(project.getPath(), 'git', 'merge', mergeCommit.getRightSHA())   
     }
 
+    static private Process performDiffAnalysisDeffault(Project project, MergeCommit mergeCommit, String optionsDiff) {
+        Process checkoutLeft = ProcessRunner.runProcess(project.getPath(), 'git', 'checkout', mergeCommit.getLeftSHA())
+        checkoutLeft.waitFor()
+
+        return ProcessRunner.runProcess(project.getPath(), 'git', 'diff', mergeCommit.getRightSHA(), optionsDiff)   
+    }
+
+    static private Boolean performDiffAnalysisByParents(Project project, String optionsDiff, String parentOne, String parentTwo, className) {
+        String localPathClass = localPathClassForDiff(getListFilesChanged(project, parentOne, parentTwo), className)
+        
+        Process checkoutParentOne = ProcessRunner.runProcess(project.getPath(), 'git', 'checkout', parentOne)
+        checkoutParentOne.waitFor()
+
+        return ProcessRunner.runProcess(project.getPath(), 'git', 'diff', parentTwo, optionsDiff+" "+localPathClass).getText() == ""   
+    }
+
+    static private String localPathClassForDiff(String[] filesList, String className){
+        String newClassName = className.replaceAll("\\.","/")
+        filesList.each{value ->
+            if (value.contains(newClassName)){
+                return value
+            }
+        }
+        return ""
+    }
+    
     static private Process performDiffAnalysis(Project project, MergeCommit mergeCommit) {
         Process checkoutLeft = ProcessRunner.runProcess(project.getPath(), 'git', 'checkout', mergeCommit.getLeftSHA())
         checkoutLeft.waitFor()
--- src/services/ExperimentalDataCollectorDynamicSemanticConflictImpl.groovy
+++ src/services/ExperimentalDataCollectorDynamicSemanticConflictImpl.groovy
@@ -6,6 +6,7 @@ import main.project.MergeCommit
 import main.project.Project
 import main.util.FileManager
 import main.util.ProcessRunner
+import main.util.MergeHelper
 import org.apache.commons.lang3.StringUtils
 
 import java.util.regex.Matcher
@@ -18,14 +19,15 @@ class ExperimentalDataCollectorDynamicSemanticConflictImpl extends ExperimentalD
 
     @Override
     protected String addHeaderLinesForOutputFile(){
-        return 'project;merge commit;left commit;right commit;base commit;className;method;local_clone;local_clone_module_analysis\n'
+        return 'project;merge commit;left commit;right commit;base commit;className;method;local_clone;local_clone_module_analysis;empty_diff_base_left;empty_diff_base_right;empty_diff_base_merge\n'
     }
     
     @Override
     protected String addMergeCommitInfoIntoOutputFile(Project project, MergeCommit mergeCommit, String className, String modifiedDeclarationSignature,
                       HashSet<Integer> leftAddedLines, HashSet<Tuple2> leftDeletedLines, HashSet<Integer> rightAddedLines,
                       HashSet<Tuple2> rightDeletedLines){
-        return "${project.getName()};${mergeCommit.getSHA()};${mergeCommit.getLeftSHA()};${mergeCommit.getRightSHA()};${mergeCommit.getAncestorSHA()};${className};${modifiedDeclarationSignature.split("\\(")[0]};${project.getFullLocalPath()};${closestModuleProjectForClass(project, className)}\n"
+        ArrayList<Boolean> emptyDiffsByParents = MergeHelper.checkForEmptyDiffByParents(project, mergeCommit, className)
+        return "${project.getName()};${mergeCommit.getSHA()};${mergeCommit.getLeftSHA()};${mergeCommit.getRightSHA()};${mergeCommit.getAncestorSHA()};${className};${modifiedDeclarationSignature.split("\\(")[0]};${project.getFullLocalPath()};${closestModuleProjectForClass(project, className)};${emptyDiffsByParents[0]};${emptyDiffsByParents[1]};${emptyDiffsByParents[2]}\n"
     }
 
     private String closestModuleProjectForClass(Project project, String className){
@@ -41,16 +43,22 @@ class ExperimentalDataCollectorDynamicSemanticConflictImpl extends ExperimentalD
             }
         }
         
-        File path = new File(System.getProperty("user.dir")+fullPathClass.replace("./","/").split(className)[0]+"pom.xml")
-        while(!isPomHere){
-            if (path.exists()){  
-                closestModule = path.toString().split("pom.xml")[0]
-                isPomHere = true
-            }else{
-                path = new File(path.toString().split(/[a-zA-Z0-9]*\/pom.xml/)[0]+"pom.xml")    
+        
+        try{
+            File path = new File(System.getProperty("user.dir")+fullPathClass.replace("./","/").split(className)[0]+"pom.xml")
+            while(!isPomHere){
+                if (path.exists()){  
+                    closestModule = path.toString().split("pom.xml")[0]
+                    isPomHere = true
+                }else{
+                    path = new File(path.toString().split(/[a-zA-Z0-9]*\/pom.xml/)[0]+"pom.xml")    
+                }
+                
             }
-            
+        } catch(Exception e1){
+            print(e1)
         }
+        
         return closestModule != "" ? closestModule : project.getFullLocalPath();
     }
 

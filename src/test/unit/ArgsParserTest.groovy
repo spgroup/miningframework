078c96843c53ba15aa82b15f0d028615b5926dd4
barbosamaathues <matheustdd@gmail.com>
2020-04-18 14:09:55 -0300

    test(#95): re-write tests in junit 5 improving exception capture

--- src/test/unit/ArgsParserTest.groovy
+++ src/test/unit/ArgsParserTest.groovy
@@ -5,7 +5,8 @@ import arguments.Arguments
 import exception.InvalidArgsException
 import org.junit.Test
 
-import static util.Assert.*
+import static org.junit.jupiter.api.Assertions.assertThrows
+import static util.Assert.assertEquals
 
 public class ArgsParserTest {
 
@@ -23,16 +24,16 @@ public class ArgsParserTest {
                          './projects.csv', 'SOOTAnalysisOutput']
         Arguments resultArgs = argsParser.parse(args)
 
-        assertEquals(resultArgs.accessKey,  'token')
-        assertEquals(resultArgs.isHelp(),  false)
-        assertEquals(resultArgs.injector.toString(),  'class injectors.StaticAnalysisConflictsDetectionModule')
-        assertEquals(resultArgs.keepProjects,  true)
-        assertEquals(resultArgs.resultsRemoteRepositoryURL,  'https://github.com/mock-project')
-        assertEquals(resultArgs.sinceDate,  '01/01/2000')
-        assertEquals(resultArgs.numOfThreads,  2)
-        assertEquals(resultArgs.untilDate,  '01/01/2000')
-        assertEquals(resultArgs.inputPath,  './projects.csv')
-        assertEquals(resultArgs.outputPath,  'SOOTAnalysisOutput')
+        assertEquals(resultArgs.accessKey, 'token')
+        assertEquals(resultArgs.isHelp(), false)
+        assertEquals(resultArgs.injector.toString(), 'class injectors.StaticAnalysisConflictsDetectionModule')
+        assertEquals(resultArgs.keepProjects, true)
+        assertEquals(resultArgs.resultsRemoteRepositoryURL, 'https://github.com/mock-project')
+        assertEquals(resultArgs.sinceDate, '01/01/2000')
+        assertEquals(resultArgs.numOfThreads, 2)
+        assertEquals(resultArgs.untilDate, '01/01/2000')
+        assertEquals(resultArgs.inputPath, './projects.csv')
+        assertEquals(resultArgs.outputPath, 'SOOTAnalysisOutput')
     }
 
     @Test
@@ -40,16 +41,16 @@ public class ArgsParserTest {
         String[] args = ['./projects.csv',]
         Arguments resultArgs = argsParser.parse(args)
 
-        assertEquals(resultArgs.accessKey,  '')
-        assertEquals(resultArgs.isHelp(),  false)
-        assertEquals(resultArgs.injector.toString(),  'class injectors.StaticAnalysisConflictsDetectionModule')
-        assertEquals(resultArgs.keepProjects,  false)
-        assertEquals(resultArgs.resultsRemoteRepositoryURL,  '')
-        assertEquals(resultArgs.sinceDate,  '')
-        assertEquals(resultArgs.numOfThreads,  1)
-        assertEquals(resultArgs.untilDate,  '')
-        assertEquals(resultArgs.inputPath,  './projects.csv')
-        assertEquals(resultArgs.outputPath,  'output')
+        assertEquals(resultArgs.accessKey, '')
+        assertEquals(resultArgs.isHelp(), false)
+        assertEquals(resultArgs.injector.toString(), 'class injectors.StaticAnalysisConflictsDetectionModule')
+        assertEquals(resultArgs.keepProjects, false)
+        assertEquals(resultArgs.resultsRemoteRepositoryURL, '')
+        assertEquals(resultArgs.sinceDate, '')
+        assertEquals(resultArgs.numOfThreads, 1)
+        assertEquals(resultArgs.untilDate, '')
+        assertEquals(resultArgs.inputPath, './projects.csv')
+        assertEquals(resultArgs.outputPath, 'output')
     }
 
     @Test
@@ -57,12 +58,12 @@ public class ArgsParserTest {
         String[] help = ['--help']
         Arguments resultArgsHelp = argsParser.parse(help)
 
-        assertEquals(resultArgsHelp.isHelp(),  true)
+        assertEquals(resultArgsHelp.isHelp(), true)
 
         String[] argh = ['-h']
         Arguments resultArgsH = argsParser.parse(argh)
 
-        assertEquals(resultArgsH.isHelp(),  true)
+        assertEquals(resultArgsH.isHelp(), true)
     }
 
     @Test
@@ -77,26 +78,45 @@ public class ArgsParserTest {
     }
 
     @Test
-    public void invalidArgument() {
-        String[] args = ['--invalid', 'invalid','./projects.csv']
+    public void invalidArgumentPassed() {
+        String[] args = ['--invalid', 'invalid', './projects.csv']
 
-        try {
+        Throwable exception = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(args)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Too many arguments passed');
-        }
+        })
+
+        String expectedMessage = "Invalid argument passed"
+        String actualMessage = exception.getMessage()
+
+        assertEquals(expectedMessage, actualMessage)
+    }
+
+    @Test
+    public void manyArgumentPassed() {
+        String[] args = ['invalid', 'invalid', './projects.csv']
+
+        Throwable exception = assertThrows(InvalidArgsException.class, () -> {
+            argsParser.parse(args)
+        })
+
+        String expectedMessage = 'Too many arguments passed'
+        String actualMessage = exception.getMessage()
+
+        assertEquals(expectedMessage, actualMessage)
     }
 
     @Test
     public void invalidInputFileArgument() {
         String[] args = ['./projects.txt']
 
-        try {
+        Throwable exception = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(args)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'The input must be a csv file');
-        }
+        })
 
+        String expectedMessage = 'The input must be a csv file'
+        String actualMessage = exception.getMessage()
+
+        assertEquals(expectedMessage, actualMessage)
     }
 
     @Test
@@ -104,11 +124,14 @@ public class ArgsParserTest {
         String inputFile = './doesNotExist.csv'
         String[] args = [inputFile]
 
-        try {
+        Throwable exception = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(args)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), "Could not find input file: ${inputFile}");
-        }
+        })
+
+        String expectedMessage = "Could not find input file: ${inputFile}"
+        String actualMessage = exception.getMessage()
+
+        assertEquals(expectedMessage, actualMessage)
     }
 
     @Test
@@ -121,132 +144,142 @@ public class ArgsParserTest {
 
     @Test
     public void sinceInvalidArgument() {
+        String expectedMessage = 'Invalid since date. You must specify it with the format DD/MM/YYYY'
+
         String[] argsSince = ['--since', '2000/01/01', './projects.csv', 'SOOTAnalysisOutput']
 
-        try {
+        Throwable argsSinceException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argsSince)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid since date. You must specify it with the format DD/MM/YYYY');
-        }
+        })
 
-        String[] argS = ['-s', 'since','./projects.csv']
+        assertEquals(expectedMessage, argsSinceException.getMessage())
 
-        try {
+        String[] argS = ['-s', 'since', './projects.csv']
+
+        Throwable argsSException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argS)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid since date. You must specify it with the format DD/MM/YYYY');
-        }
+        })
+
+        assertEquals(expectedMessage, argsSException.getMessage())
     }
 
     @Test
     public void untilInvalidArgument() {
+        String expectedMessage = 'Invalid since date. You must specify it with the format DD/MM/YYYY'
+
         String[] argsUntil = ['--until', '2000/01/01', './projects.csv', 'SOOTAnalysisOutput']
 
-        try {
+        Throwable argsUntilException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argsUntil)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid since date. You must specify it with the format DD/MM/YYYY');
-        }
+        })
 
-        String[] argU = ['-u', 'until','./projects.csv']
+        assertEquals(expectedMessage, argsUntilException.getMessage())
 
-        try {
+        String[] argU = ['-u', 'until', './projects.csv']
+
+        Throwable argsUException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argU)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid since date. You must specify it with the format DD/MM/YYYY');
-        }
+        })
+
+        assertEquals(expectedMessage, argsUException.getMessage())
     }
 
     @Test
     public void injectorInvalidArgument() {
+        String expectedMessage = 'Invalid injector class. be sure it is in your classpath'
+
         String[] argsInjector = ['--injector', 'app/Main.groovy', './projects.csv', 'SOOTAnalysisOutput']
 
-        try {
+        Throwable argsInjectorException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argsInjector)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid injector class. be sure it is in your classpath');
-        }
+        })
+
+        assertEquals(expectedMessage, argsInjectorException.getMessage())
 
         String[] argI = ['-i', 'app/Main.groovy', './projects.csv']
 
-        try {
+        Throwable argsIException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argI)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid injector class. be sure it is in your classpath');
-        }
+        })
+
+        assertEquals(expectedMessage, argsIException.getMessage())
     }
 
     @Test
     public void pushInexistentRepositoryArgument() {
+        String expectedMessage = 'Inexistent remote git repository.'
+
         String[] argsPush = ['--push', 'https://github.com/spgroup/invalidrepo', './projects.csv', 'SOOTAnalysisOutput']
 
-        try {
+        Throwable argsPushException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argsPush)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Inexistent remote git repository.');
-        }
+        })
 
-        String[] argP = ['-p', 'https://github.com/spgroup/invalidrepo','./projects.csv']
+        assertEquals(expectedMessage, argsPushException.getMessage())
 
-        try {
+        String[] argP = ['-p', 'https://github.com/spgroup/invalidrepo', './projects.csv']
+
+        Throwable argsPException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argP)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Inexistent remote git repository.');
-        }
+        })
+
+        assertEquals(expectedMessage, argsPException.getMessage())
     }
 
     @Test
     public void pushInvalidURLArgument() {
+        String expectedMessage = 'Invalid url.'
+
         String[] argsPush = ['--push', 'invalidrepourl', './projects.csv', 'SOOTAnalysisOutput']
 
-        try {
+        Throwable argsPushException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argsPush)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid url.');
-        }
+        })
 
-        String[] argP = ['-p', 'invalidrepourl','./projects.csv']
+        assertEquals(expectedMessage, argsPushException.getMessage())
 
-        try {
+        String[] argP = ['-p', 'invalidrepourl', './projects.csv']
+
+        Throwable argsPException = assertThrows(InvalidArgsException.class, () -> {
             argsParser.parse(argP)
-        } catch (InvalidArgsException e) {
-            assertEquals(e.getMessage(), 'Invalid url.');
-        }
+        })
+
+        assertEquals(expectedMessage, argsPException.getMessage())
     }
 
     @Test
     public void repeatedArgument() {
-        String[] accessKey = ['--access-key', 'token', '-a', 'token',  './projects.csv']
-        Arguments resultArgsAccessKey =  argsParser.parse(accessKey)
+        String[] accessKey = ['--access-key', 'token', '-a', 'token', './projects.csv']
+        Arguments resultArgsAccessKey = argsParser.parse(accessKey)
 
         String[] help = ['--help', '-h', './projects.csv']
-        Arguments resultArgsHelp =  argsParser.parse(help)
+        Arguments resultArgsHelp = argsParser.parse(help)
 
-        String[] injector = ['--injector', 'injectors.StaticAnalysisConflictsDetectionModule', '-i', 'injectors.StaticAnalysisConflictsDetectionModule',  './projects.csv',]
-        Arguments resultArgsInjector =  argsParser.parse(injector)
+        String[] injector = ['--injector', 'injectors.StaticAnalysisConflictsDetectionModule', '-i', 'injectors.StaticAnalysisConflictsDetectionModule', './projects.csv',]
+        Arguments resultArgsInjector = argsParser.parse(injector)
 
-        String[] keepProjects = ['--keep-projects', '-k',  './projects.csv']
-        Arguments resultArgsKeepProjects =  argsParser.parse(keepProjects)
+        String[] keepProjects = ['--keep-projects', '-k', './projects.csv']
+        Arguments resultArgsKeepProjects = argsParser.parse(keepProjects)
 
-        String[] push = ['--push', 'https://github.com/mock-project', '-p', 'https://github.com/mock-project',  './projects.csv']
-        Arguments resultArgsPush =  argsParser.parse(push)
+        String[] push = ['--push', 'https://github.com/mock-project', '-p', 'https://github.com/mock-project', './projects.csv']
+        Arguments resultArgsPush = argsParser.parse(push)
 
-        String[] since = ['--since', '01/01/2000', '-s', '01/01/2000',  './projects.csv']
-        Arguments resultArgsSince =  argsParser.parse(since)
+        String[] since = ['--since', '01/01/2000', '-s', '01/01/2000', './projects.csv']
+        Arguments resultArgsSince = argsParser.parse(since)
 
-        String[] threads = ['--threads', 2, '-t', 2,  './projects.csv']
-        Arguments resultArgsThreads =  argsParser.parse(threads)
+        String[] threads = ['--threads', 2, '-t', 2, './projects.csv']
+        Arguments resultArgsThreads = argsParser.parse(threads)
 
-        String[] until = ['--until', '01/01/2000', '-u', '01/01/2000',  './projects.csv', 'SOOTAnalysisOutput']
-        Arguments resultArgsUntil =  argsParser.parse(until)
+        String[] until = ['--until', '01/01/2000', '-u', '01/01/2000', './projects.csv', 'SOOTAnalysisOutput']
+        Arguments resultArgsUntil = argsParser.parse(until)
 
-        assertEquals(resultArgsAccessKey.accessKey,  'token')
-        assertEquals(resultArgsHelp.isHelp(),  true)
-        assertEquals(resultArgsInjector.injector.toString(),  'class injectors.StaticAnalysisConflictsDetectionModule')
-        assertEquals(resultArgsKeepProjects.keepProjects,  true)
-        assertEquals(resultArgsPush.resultsRemoteRepositoryURL,  'https://github.com/mock-project')
-        assertEquals(resultArgsSince.sinceDate,  '01/01/2000')
-        assertEquals(resultArgsThreads.numOfThreads,  2)
-        assertEquals(resultArgsUntil.untilDate,  '01/01/2000')
+        assertEquals(resultArgsAccessKey.accessKey, 'token')
+        assertEquals(resultArgsHelp.isHelp(), true)
+        assertEquals(resultArgsInjector.injector.toString(), 'class injectors.StaticAnalysisConflictsDetectionModule')
+        assertEquals(resultArgsKeepProjects.keepProjects, true)
+        assertEquals(resultArgsPush.resultsRemoteRepositoryURL, 'https://github.com/mock-project')
+        assertEquals(resultArgsSince.sinceDate, '01/01/2000')
+        assertEquals(resultArgsThreads.numOfThreads, 2)
+        assertEquals(resultArgsUntil.untilDate, '01/01/2000')
     }
 }

3faa4464716301511b59a64b16be063bc3b55757
Galileu Santos <galilasmb@gmail.com>
2020-04-27 14:43:02 -0300

    Update Tests

---
+++ src/test/integration/output/files/mergeconflict/b610225c36e1eb020b9899be907e06b96a19c1c0/Test/merge.java
@@ -0,0 +1,15 @@
+class Test {
+	public void mergeConflictOne() {
+		int x = 2;
+	}
+
+	public void mergeConflictTwo () {
+		int y = 1;
+	}
+
+	public void noConflict () {
+		int z = 0;
+		int x = 0;
+		int y = 0;
+	}
+}
---
+++ src/test/integration/output/files/mergeconflict/c00294fb7135e5bdcd6f7c3417405aea75b66444/Test/merge.java
@@ -0,0 +1,27 @@
+class Test {
+	public void twoConflictsInOneFile1() {
+		int x = 2;
+	}
+
+	public void mergeConflictOne() {
+		int x = 2;
+	}
+
+	public void mergeConflictTwo () {
+		int y = 1;
+	}
+
+	public void noConflict () {
+		int z = 0;
+		int x = 0;
+		int y = 0;
+	}
+
+	public void multipleConflicts () {
+		int x = 2;
+	}
+
+	public void twoConflictsInOneFile2() {
+		int x = 2;
+	}
+}

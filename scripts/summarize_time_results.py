dd3ca620427e8ff5fb49f6bece94c50efab148e5
Galileu Santos <galileu.jesus@lais.huol.ufrn.br>
2023-04-27 18:31:39 -0300

    chore: comments adjusts

--- scripts/summarize_time_results.py
+++ scripts/summarize_time_results.py
@@ -64,7 +64,7 @@ class ResultAnalysis:
 
 		self.results = mean_line
 	
-	#Sum the columns from files	
+	# Sum the columns from files
 	def sum_columns(self):
 		total = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0}
 		num_lines = self.dataframes[0].shape[0]

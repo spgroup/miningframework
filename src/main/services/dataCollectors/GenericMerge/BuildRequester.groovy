a26d6209bd201c55281cbb9e3f5bfe66b65c66a1
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-05-25 20:50:41 -0400

    force true

--- src/main/services/dataCollectors/GenericMerge/BuildRequester.groovy
+++ src/main/services/dataCollectors/GenericMerge/BuildRequester.groovy
@@ -29,6 +29,7 @@ class BuildRequester {
     private static void replaceFilesInProject(Project project, MergeCommit mergeCommit, List<Path> mergeScenarios, String toReplaceFile) {
         mergeScenarios.stream()
                 .filter {
+                    return true;
                     def fileExists = new File(getSource(it, toReplaceFile).toAbsolutePath().toString()).isFile();
                     println "Checking if ${getSource(it, toReplaceFile).toAbsolutePath()} exists -> ${fileExists}"
                     return fileExists;

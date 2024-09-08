ef43c879985e51d16a6139f39696fbba9aaf81f4
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-09-08 16:22:29 -0300

    fix: simplify code

--- src/main/services/dataCollectors/GenericMerge/model/MergeScenarioResult.groovy
+++ src/main/services/dataCollectors/GenericMerge/model/MergeScenarioResult.groovy
@@ -1,17 +1,7 @@
 package services.dataCollectors.GenericMerge.model
 
 enum MergeScenarioResult {
-    SUCCESS_WITHOUT_CONFLICTS("SUCCESS_WITHOUT_CONFLICTS"),
-    SUCCESS_WITH_CONFLICTS("SUCCESS_WITH_CONFLICTS"),
-    TOOL_ERROR("TOOL_ERROR");
-
-    String value
-
-    private MergeScenarioResult(String value) {
-        this.value = value
-    }
-
-    static from(String value) {
-        return new MergeScenarioResult(value)
-    }
+    SUCCESS_WITHOUT_CONFLICTS,
+    SUCCESS_WITH_CONFLICTS,
+    TOOL_ERROR
 }

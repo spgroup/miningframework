fcf3971758ed7c8cad1b1bbced7d6db8e4629dd0
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-09-08 17:00:20 -0300

    fix: tweak code

--- src/main/services/commitFilters/IsInCommitListFilter.groovy
+++ src/main/services/commitFilters/IsInCommitListFilter.groovy
@@ -1,6 +1,8 @@
 package services.commitFilters
 
 import interfaces.CommitFilter
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import project.MergeCommit
 import project.Project
 
@@ -11,36 +13,29 @@ import static com.xlson.groovycsv.CsvParser.parseCsv
  * @provides: returns true if the passed mergeCommit SHA is in the commits.csv file
  */
 class IsInCommitListFilter implements CommitFilter {
+    private static Logger LOG = LogManager.getLogger(IsInCommitListFilter.class)
 
     List<String> commitList
 
     IsInCommitListFilter() {
         File commitsFile = new File("./commits.csv")
-        this.commitList = parseCommitList(commitsFile)
+        this.commitList = commitsFile.exists() ? parseCommitList(commitsFile) : new ArrayList<>()
     }
 
     @Override
     boolean applyFilter(Project project, MergeCommit mergeCommit) {
-        return isInCommitList(mergeCommit)
+        return commitList.isEmpty() || commitList.contains(mergeCommit.getSHA())
     }
 
-    private List<String> parseCommitList (File commitsFile) {
+    private List<String> parseCommitList(File commitsFile) {
         ArrayList<String> commitList = new ArrayList<String>()
         def iterator = parseCsv(commitsFile.getText())
 
         for (line in iterator) {
+            LOG.trace("Registering ${line["commitSHA"]} as a valid commit SHA in list")
             commitList.add(line["commitSHA"])
         }
 
         return commitList
-    }
-
-    private boolean isInCommitList (MergeCommit mergeCommit) {
-        for (commit in commitList) {
-            if (mergeCommit.getSHA() == commit) {
-                return true;
-            }
-        }
-        return false;
     }
 }

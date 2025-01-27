97c6d07ec4be1ecb42fc54ab9060fe4c29799e84
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-01-26 21:06:06 -0300

    feat: run only spork

--- src/main/injectors/GenericMergeModule.groovy
+++ src/main/injectors/GenericMergeModule.groovy
@@ -8,7 +8,10 @@ import interfaces.OutputProcessor
 import interfaces.ProjectProcessor
 import services.commitFilters.MutuallyModifiedFilesCommitFilter
 import services.dataCollectors.common.CompareScenarioMergeConflictsDataCollector
+import services.dataCollectors.common.RunDataCollectorsInParallel
 import services.dataCollectors.common.SyntacticallyCompareScenarioFilesDataCollector
+import services.dataCollectors.fileSyntacticNormalization.SporkFileSyntacticNormalizationDataCollector
+import services.dataCollectors.mergeToolExecutors.SporkMergeToolExecutorDataCollector
 import services.outputProcessors.EmptyOutputProcessor
 import services.projectProcessors.DummyProjectProcessor
 
@@ -22,32 +25,20 @@ class GenericMergeModule extends AbstractModule {
 
         // Run the merge tools on the scenarios
 //        dataCollectorBinder.addBinding().to(LastMergeMergeToolExecutorDataCollector.class)
-//        dataCollectorBinder.addBinding().to(SporkMergeToolExecutorDataCollector.class)
+        dataCollectorBinder.addBinding().to(SporkMergeToolExecutorDataCollector.class)
 //        dataCollectorBinder.addBinding().to(MergirafMergeToolExecutorDataCollector.class)
-//
-//        dataCollectorBinder.addBinding().toInstance(new SporkFileSyntacticNormalizationDataCollector("merge.mergiraf.java", "merge.mergiraf.spork_normalized.java"))
-//        dataCollectorBinder.addBinding().toInstance(new SyntacticallyCompareScenarioFilesDataCollector("merge.spork.spork_normalized.java", "merge.mergiraf.spork_normalized.java"))
 
-//        dataCollectorBinder.addBinding().toInstance(new RunDataCollectorsInParallel(new ArrayList<DataCollector>([new SpoonFormatFileSyntacticNormalizationDataCollector("merge.java", "merge.spoon_normalized.java"),
-//                                                                                                                  new SpoonFormatFileSyntacticNormalizationDataCollector("merge.last_merge.java", "merge.last_merge.spoon_normalized.java"),
-//                                                                                                                  new SpoonFormatFileSyntacticNormalizationDataCollector("merge.spork.java", "merge.spork.spoon_normalized.java")])))
-
-//        dataCollectorBinder.addBinding().toInstance(new RunDataCollectorsInParallel(new ArrayList<DataCollector>([new SporkFileSyntacticNormalizationDataCollector("merge.java", "merge.spork_normalized.java"),
-//                                                                                                                  new SporkFileSyntacticNormalizationDataCollector("merge.last_merge.java", "merge.last_merge.spork_normalized.java"),
-//                                                                                                                  new SporkFileSyntacticNormalizationDataCollector("merge.spork.java", "merge.spork.spork_normalized.java")])))
+        dataCollectorBinder.addBinding().toInstance(new SporkFileSyntacticNormalizationDataCollector("merge.spork.java", "merge.spork.spork_normalized.java"))
 
 
-//        dataCollectorBinder.addBinding().toInstance(new RunDataCollectorsInParallel(new ArrayList<DataCollector>([
-        // Syntactically compare both Spork and Last Merge files
-//                new SyntacticallyCompareScenarioFilesDataCollector("merge.spork.spork_normalized.java", "merge.last_merge.spork_normalized.java"),
-        // Syntactically compare the tools with merge commit
-//                new SyntacticallyCompareScenarioFilesDataCollector("merge.spork.spork_normalized.java", "merge.spork_normalized.java"),
-//                new SyntacticallyCompareScenarioFilesDataCollector("merge.last_merge.spork_normalized.java", "merge.spork_normalized.java"),
+        dataCollectorBinder.addBinding().toInstance(new RunDataCollectorsInParallel(new ArrayList<DataCollector>([
+//         Syntactically compare both Spork and Last Merge files
+new SyntacticallyCompareScenarioFilesDataCollector("merge.spork.spork_normalized.java", "merge.last_merge.spork_normalized.java"),
+// Syntactically compare the tools with merge commit
+new SyntacticallyCompareScenarioFilesDataCollector("merge.spork.spork_normalized.java", "merge.spork_normalized.java"),
 //                 Run comparisons between conflicts themselves
-//                new CompareScenarioMergeConflictsDataCollector("merge.last_merge.java", "merge.spork.java")])))
+new CompareScenarioMergeConflictsDataCollector("merge.last_merge.java", "merge.spork.java")])))
 
-        dataCollectorBinder.addBinding().toInstance(new CompareScenarioMergeConflictsDataCollector("merge.mergiraf.java", "merge.spork.java"))
-        dataCollectorBinder.addBinding().toInstance(new SyntacticallyCompareScenarioFilesDataCollector("merge.java", "merge.mergiraf.java"))
 
 //        dataCollectorBinder.addBinding().toInstance(new RequestBuildForRevisionWithFilesDataCollector("merge.last_merge.java"))
 //        dataCollectorBinder.addBinding().toInstance(new RequestBuildForRevisionWithFilesDataCollector("merge.spork.java"))

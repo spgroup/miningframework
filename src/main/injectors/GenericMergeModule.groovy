8ef4d07fa295251f7be858219935894f8f5ac677
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-02-06 12:24:45 -0300

    feat: adjust syntactical comparisons

--- src/main/injectors/GenericMergeModule.groovy
+++ src/main/injectors/GenericMergeModule.groovy
@@ -8,8 +8,7 @@ import interfaces.OutputProcessor
 import interfaces.ProjectProcessor
 import services.commitFilters.MutuallyModifiedFilesCommitFilter
 import services.dataCollectors.common.RunDataCollectorsInParallel
-import services.dataCollectors.fileSyntacticNormalization.JDimeFileSyntacticNormalizationDataCollector
-import services.dataCollectors.fileSyntacticNormalization.SporkFileSyntacticNormalizationDataCollector
+import services.dataCollectors.common.SyntacticallyCompareScenarioFilesDataCollector
 import services.outputProcessors.EmptyOutputProcessor
 import services.projectProcessors.DummyProjectProcessor
 
@@ -27,13 +26,27 @@ class GenericMergeModule extends AbstractModule {
 //        dataCollectorBinder.addBinding().to(SporkMergeToolExecutorDataCollector.class)
 //        dataCollectorBinder.addBinding().to(MergirafMergeToolExecutorDataCollector.class)
 
-        // Run, in parallel, normalizations on resulting files
+//        // Run, in parallel, normalizations on resulting files
+//        dataCollectorBinder.addBinding().toInstance(new RunDataCollectorsInParallel([
+//                new JDimeFileSyntacticNormalizationDataCollector("merge.java", "merge.jdime_normalized.java"),
+//                new JDimeFileSyntacticNormalizationDataCollector("merge.last_merge.java", "merge.last_merge.jdime_normalized.java"),
+//                new SporkFileSyntacticNormalizationDataCollector("merge.java", "merge.spork_normalized.java"),
+//                new SporkFileSyntacticNormalizationDataCollector("merge.spork.java", "merge.spork.spork_normalized.java"),
+//                new SporkFileSyntacticNormalizationDataCollector("merge.mergiraf.java", "merge.mergiraf.spork_normalized.java"),
+//        ]))
+
+        // Run, in parallel, syntactical comparisons between files
         dataCollectorBinder.addBinding().toInstance(new RunDataCollectorsInParallel([
-                new JDimeFileSyntacticNormalizationDataCollector("merge.java", "merge.jdime_normalized.java"),
-                new JDimeFileSyntacticNormalizationDataCollector("merge.last_merge.java", "merge.last_merge.jdime_normalized.java"),
-                new SporkFileSyntacticNormalizationDataCollector("merge.java", "merge.spork_normalized.java"),
-                new SporkFileSyntacticNormalizationDataCollector("merge.spork.java", "merge.spork.spork_normalized.java"),
-                new SporkFileSyntacticNormalizationDataCollector("merge.mergiraf.java", "merge.mergiraf.spork_normalized.java"),
+                // With merge commits
+                new SyntacticallyCompareScenarioFilesDataCollector("merge.java", "merge.last_merge.java"),
+                new SyntacticallyCompareScenarioFilesDataCollector("merge.jdime_normalized.java", "merge.jdime.java"),
+                new SyntacticallyCompareScenarioFilesDataCollector("merge.spork_normalized.java", "merge.spork.spork_normalized.java"),
+                new SyntacticallyCompareScenarioFilesDataCollector("merge.mergiraf.java", "merge.last_merge.java"),
+
+                // Between tools themselves
+                new SyntacticallyCompareScenarioFilesDataCollector("merge.jdime.java", "merge.last_merge.jdime_normalized.java"),
+                new SyntacticallyCompareScenarioFilesDataCollector("merge.mergiraf.spork_normalized.java", "merge.spork.spork_normalized.java"),
+                new SyntacticallyCompareScenarioFilesDataCollector("merge.mergiraf.java", "merge.last_merge.java"),
         ]))
 
         Multibinder<OutputProcessor> outputProcessorBinder = Multibinder.newSetBinder(binder(), OutputProcessor.class)
